595cef4cc6b2a3d9a2e770962a1d737ac54a1a4a
AnshumanG20 <faaltuno9@gmail.com>
2023-08-10 14:11:39 +0530

    changes pushed

---
+++ property_build/assets/GovDcbReport-877d7a4d.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-c815f2b8.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
---
+++ property_build/assets/RazorpayPaymentScreen-d213c373.js
@@ -0,0 +1,1 @@
+import"./index-c815f2b8.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
