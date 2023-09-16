21120ec9fd386d7e53899855edaaccc3cd0a9024
AnshumanG20 <faaltuno9@gmail.com>
2023-09-16 16:21:55 +0530

    changes

---
+++ advertisement_build/assets/useSetTitle-23154462.js
@@ -0,0 +1,1 @@
+import{v as s,p as o,r as a}from"./index-11d41df0.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
--- advertisement_build_old/assets/useSetTitle-bbfc54a8.js
+++
@@ -1,1 +0,0 @@
-import{x as s,p as o,r as a}from"./index-994ab0de.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
