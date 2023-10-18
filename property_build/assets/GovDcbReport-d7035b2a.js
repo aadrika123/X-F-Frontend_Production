0d80af38dcfbce235ec36a64d6cc7ec781d1f128
aadrika123 <it.info@aadrikaenterprises.com>
2023-10-18 18:47:26 +0530

    changes

---
+++ property_build/assets/GovDcbReport-d7035b2a.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-e34bdbb6.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- react-server.js
+++ react-server.js
@@ -20,6 +20,7 @@ app.use(express.static("advertisement_build"));
 app.use(express.static("citizen_mb_build"));
 app.use(express.static("tanker_admin_build"));
 app.use(express.static("grievance_build"));
+app.use(express.static("daily_license_build"));
 
 //actual routes
 app.get("/citizen", (req, res) => {
@@ -77,6 +78,12 @@ app.get("/grievance", (req, res) => {
 });
 app.get("/grievance/*", (req, res) => {
   res.sendFile(path.join(__dirname, "grievance_build", "index.html"));
+});
+app.get("/daily-license-app", (req, res) => {
+  res.sendFile(path.join(__dirname, "daily_license_build", "index.html"));
+});
+app.get("/daily-license-app/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "daily_license_build", "index.html"));
 });
 // start express server on port 80
 app.listen(80, () => {
