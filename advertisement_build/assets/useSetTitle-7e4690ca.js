1cc15a8b91e33d5bfb850c8db73fbd7cf04cfb5f
aadrika123 <it.info@aadrikaenterprises.com>
2023-11-02 18:07:54 +0530

    changes

---
+++ advertisement_build/assets/useSetTitle-7e4690ca.js
@@ -0,0 +1,1 @@
+import{R as s,U as o,r as a}from"./index-f11f8618.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
---
+++ property_build/assets/RazorpayPaymentScreen-6aeb1cf8.js
@@ -0,0 +1,1 @@
+import"./index-763265b4.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
