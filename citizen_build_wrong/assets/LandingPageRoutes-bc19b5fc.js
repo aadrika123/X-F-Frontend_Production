aafb2a69d8c9d16fcfaf1ec9d123f35017b511fe
aadrika123 <it.info@aadrikaenterprises.com>
2023-11-18 19:17:00 +0530

    change

--- citizen_build_wrong/assets/LandingPageRoutes-bc19b5fc.js
+++
@@ -1,1 +0,0 @@
-import{j as t,aj as a,ak as e,al as s}from"./index-a511550f.js";const n=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{n as default};
--- citizen_build_wrong/assets/login-341c8dbe.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/login-46b9cc9d.svg";export{s as l};
