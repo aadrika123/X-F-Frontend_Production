e6d83f9419b401ea52e1100fe2d62439bc5be27a
aadrika123 <it.info@aadrikaenterprises.com>
2023-11-18 11:43:31 +0530

    changes

---
+++ daily_license_build/assets/swatchBharat-69854ede.js
@@ -0,0 +1,1 @@
+const a="/assets/swatchBharat-9350afb8.png";export{a as s};
---
+++ daily_license_build/assets/swatchBharat-legacy-fdb351e9.js
@@ -0,0 +1,1 @@
+System.register([],(function(t,e){"use strict";return{execute:function(){t("s","/assets/swatchBharat-9350afb8.png")}}}));
