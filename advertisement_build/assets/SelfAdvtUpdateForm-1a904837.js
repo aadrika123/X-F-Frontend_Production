1c24b98d936a9c23d95506b5558c0a803d518e4a
aadrika123 <it.info@aadrikaenterprises.com>
2023-11-30 16:36:06 +0530

    changes

---
+++ advertisement_build/assets/SelfAdvtUpdateForm-1a904837.js
@@ -0,0 +1,1 @@
+import{h as a,j as d}from"./index-5e7d8a49.js";function t(){const{id:e}=a();return console.log("can edit param received id",e),d("div",{children:"SelfAdvtUpdateForm"})}export{t as default};
