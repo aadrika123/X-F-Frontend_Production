7a003e3e99014e738351efa9534d7951f016e5db
aadrika123 <it.info@aadrikaenterprises.com>
2023-12-02 18:37:08 +0530

    changes

---
+++ advertisement_build/assets/SelfAdvtUpdateForm-c614fa26.js
@@ -0,0 +1,1 @@
+import{h as a,j as d}from"./index-f9c0f641.js";function t(){const{id:e}=a();return console.log("can edit param received id",e),d("div",{children:"SelfAdvtUpdateForm"})}export{t as default};
---
+++ advertisement_build/assets/useSetTitle-051a5de3.js
@@ -0,0 +1,1 @@
+import{R as s,U as o,r as a}from"./index-f9c0f641.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
