95f1735141b913eb4379af381e398d0d9c7c034f
aadrika123 <it.info@aadrikaenterprises.com>
2023-12-06 12:07:09 +0530

    changes

---
+++ property_build/assets/QrCode-c7cb3d6b.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-da0b8a3a.js";import{Q as o}from"./index-11258231.js";function i(r){return e.jsx(o,{value:r.url,size:r.size})}export{i as Q};
---
+++ property_build/assets/RazorpayPaymentScreen-b7bdfaa2.js
@@ -0,0 +1,1 @@
+import{br as o}from"./index-da0b8a3a.js";let t=o("token");console.log("token at basic details is post method...",t);
