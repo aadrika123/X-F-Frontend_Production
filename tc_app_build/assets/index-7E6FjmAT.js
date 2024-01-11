0e94dc938be2490bba4ec176c36da72f64d6973d
Talib Hussain <talibplay111@gmail.com>
2024-01-11 11:49:04 +0530

    updates

--- tc_app_build/assets/index-7E6FjmAT.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-YlgYywsm.js";function t(){return e.jsx("div",{children:"WaterPage"})}export{t as default};
--- tc_app_build/assets/index-legacy--tB87Kxz.js
+++
@@ -1,1 +0,0 @@
-System.register(["./index-legacy-fcJD79JR.js"],(function(e,t){"use strict";var n;return{setters:[function(e){n=e.j}],execute:function(){e("default",(function(){return n.jsx("div",{children:"WaterPage"})}))}}}));
