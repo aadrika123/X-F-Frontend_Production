358ec5671d08259a8ccc7c87a8ba2e6cd2be4700
Talib Hussain <talibplay111@gmail.com>
2024-01-25 18:29:00 +0530

    updates

---
+++ daily_license_build/assets/ProtectedRoutes-Jtm6Jeku.js
@@ -0,0 +1,1 @@
+import{r as e,M as o,j as t,N as a,O as r}from"./index-QmcauDG8.js";const n=s=>(e.useContext(o),localStorage.getItem("token")==null||localStorage.getItem("token")==""||localStorage.getItem("token")==null?t.jsx(a,{to:"/"}):t.jsx(r,{}));export{n as default};
---
+++ daily_license_build/assets/ProtectedRoutes-legacy-Y6Ut5YFH.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-0gZndjrK.js"],(function(t,e){"use strict";var n,o,r,l,u;return{setters:[function(t){n=t.r,o=t.M,r=t.j,l=t.N,u=t.O}],execute:function(){t("default",(function(t){return n.useContext(o),null==localStorage.getItem("token")||""==localStorage.getItem("token")||null==localStorage.getItem("token")?r.jsx(l,{to:"/"}):r.jsx(u,{})}))}}}));
