a7aec92cfcd3bbd8b7f6f24f9b0de1604464313c
Talib Hussain <talibplay111@gmail.com>
2024-01-29 23:04:01 +0530

    updates

--- tc_app_build_old/assets/FormProvider--m3L4Toi.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-6omHlbjT.js";import{a as t,b as e}from"./formik.esm-skjjBd88.js";function n({children:i,formik:r,className:m}){return o.jsx(t,{value:r,children:o.jsx(e,{onSubmit:r.handleSubmit,className:m,children:i})})}export{n as F};
