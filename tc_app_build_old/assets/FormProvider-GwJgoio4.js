5fa9fe92021477d34856a775966578b84587cb56
Talib Hussain <talibplay111@gmail.com>
2024-01-30 09:56:30 +0530

    updates

--- tc_app_build_old/assets/FormProvider-GwJgoio4.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-26-p7w-4.js";import{a as t,b as e}from"./formik.esm-gFKTrVOh.js";function n({children:i,formik:r,className:m}){return o.jsx(t,{value:r,children:o.jsx(e,{onSubmit:r.handleSubmit,className:m,children:i})})}export{n as F};
--- tc_app_build_old/assets/usePathWisePermission-NdWuWjKi.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-26-p7w-4.js";function a(u){var n;const o=t(),r=(n=o==null?void 0:o.userRoutes)==null?void 0:n.map(s=>s==null?void 0:s.pages).flat(1);return{result:r==null?void 0:r.find(s=>s.route==u)}}export{a as u};
