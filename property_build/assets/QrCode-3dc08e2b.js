a3df25b105351f6a7bc1f3524839981ce1b51ee4
akshay2s <akshay.aadrika@gmail.com>
2024-03-14 18:51:08 +0530

    'changes'

--- property_build/assets/QrCode-3dc08e2b.js
+++
@@ -1,1 +0,0 @@
-import{j as r,ay as s}from"./index-8d277f67.js";function t(e){return r.jsx(s,{value:e.url,size:e.size})}export{t as Q};
--- property_build/assets/RazorpayPaymentScreen-0d27c5ec.js
+++
@@ -1,1 +0,0 @@
-import"./index-8d277f67.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
