3f741dafe73ae2459499dcd5dcbc6b3afa9350a8
akshay2s <akshay.aadrika@gmail.com>
2024-03-19 05:38:10 +0530

    changes to racky bhai

---
+++ property_build/assets/RazorpayPaymentScreen-acdf7c4d.js
@@ -0,0 +1,1 @@
+import"./index-025cc29f.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
---
+++ property_build_live/assets/GovDcbReport-f722bec8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-11eb40a4.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
