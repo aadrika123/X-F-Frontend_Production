741ce6f5f9ba3b9c548fc7c02d0be9897308c8e1
akshay2s <akshay.aadrika@gmail.com>
2024-03-22 14:00:55 +0530

    changes for tc app

---
+++ tc_app_build/assets/FormProvider-4hRvP7IH.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DT14CbOw.js";import{a as s,b as a}from"./formik.esm-MEJnthxH.js";function i({children:e,formik:r,className:o}){return t.jsx(s,{value:r,children:t.jsx(a,{onSubmit:r==null?void 0:r.handleSubmit,className:o,children:e})})}export{i as F};
