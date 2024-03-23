ae11cb3dcccbc24e4a3e4756d77d5decdeb16b37
akshay2s <akshay.aadrika@gmail.com>
2024-03-23 15:55:52 +0530

    changes

--- citizen_build/assets/LandingPageRoutes-a1f743ed.js
+++
@@ -1,1 +0,0 @@
-import{j as t,al as a,am as e,an as s}from"./index-58de3b88.js";const o=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{o as default};
