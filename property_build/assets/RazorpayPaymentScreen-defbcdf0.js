7639fb4b953b6b2c4cbb27d04ddcc4095f5213a2
Talib Hussain <talibplay111@gmail.com>
2024-05-02 13:36:26 +0530

    Changes

---
+++ property_build/assets/RazorpayPaymentScreen-defbcdf0.js
@@ -0,0 +1,1 @@
+import"./index-f317ffa6.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
---
+++ property_build/assets/team-a0256d3f.js
@@ -0,0 +1,1 @@
+const a="/assets/water-0f4aab56.png",s="/assets/team-91a32e8a.png";export{s as t,a as w};
