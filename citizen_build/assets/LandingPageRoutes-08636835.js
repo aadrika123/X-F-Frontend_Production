9afc447d697a9109d59447fae183757128cb4c45
Talib Hussain <talibplay111@gmail.com>
2024-05-04 14:02:50 +0530

    changes

---
+++ citizen_build/assets/LandingPageRoutes-08636835.js
@@ -0,0 +1,1 @@
+import{j as t,al as a,am as e,an as s}from"./index-cd8f890b.js";const o=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{o as default};
