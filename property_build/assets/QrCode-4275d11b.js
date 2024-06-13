219c9684120a12162f4edd48d88440d5404e7878
akshay2s <akshay.aadrika@gmail.com>
2024-06-13 12:06:44 +0530

    changes

---
+++ property_build/assets/QrCode-4275d11b.js
@@ -0,0 +1,1 @@
+import{j as r,aD as s}from"./index-0795527d.js";function t(e){return r.jsx(s,{value:e.url,size:e.size})}export{t as Q};
--- property_build_old/assets/RazorpayPaymentScreen-4fa3049f.js
+++
@@ -1,1 +0,0 @@
-import"./index-c7c26940.js";window.localStorage.getItem("token");
