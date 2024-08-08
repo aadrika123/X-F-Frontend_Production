7891f720db97c22039833d4de960dc09a05f6a83
akshay2s <akshay.aadrika@gmail.com>
2024-08-08 12:35:19 +0530

    hnages

---
+++ citizen_build_old/assets/LandingPageRoutes-70d159cc.js
@@ -0,0 +1,1 @@
+import{j as t,ap as a,aq as e,ar as s}from"./index-f28bac82.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{r as default};
--- citizen_build_old/assets/LandingPageRoutes-a4c77a24.js
+++
@@ -1,1 +0,0 @@
-import{j as t,ap as a,aq as e,ar as s}from"./index-8610f755.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(e,{path:"/about",element:t.jsx(s,{})})})});export{r as default};
