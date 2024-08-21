c1260d179e813140736c1731764d80790dc6d740
akshay2s <akshay.aadrika@gmail.com>
2024-08-21 15:20:42 +0530

    changes

---
+++ property_build/assets/GovDcbReport-2b6a76b1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-48b3dac3.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
---
+++ property_build/assets/RazorpayPaymentScreen-e5aa4bd7.js
@@ -0,0 +1,1 @@
+import"./index-48b3dac3.js";window.localStorage.getItem("token");
