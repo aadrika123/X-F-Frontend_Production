b6ec08d1f440b7e07c74249886d166348c8e616c
akshay2s <akshay.aadrika@gmail.com>
2024-11-27 13:26:29 +0530

    fdsfs

---
+++ tc_app_build/assets/FormProvider-legacy-6BHIXKAP.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-6C_WZFrB.js","./formik.esm-legacy-L7X0greu.js"],(function(e,n){"use strict";var i,t,r;return{setters:[function(e){i=e.j},function(e){t=e.a,r=e.b}],execute:function(){e("F",(function(e){var n=e.children,s=e.formik,c=e.className;return i.jsx(t,{value:s,children:i.jsx(r,{onSubmit:null==s?void 0:s.handleSubmit,className:c,children:n})})}))}}}));
