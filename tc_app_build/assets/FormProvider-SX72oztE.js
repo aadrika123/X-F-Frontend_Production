e68f103b15d30cfc59a4b58b6d4a4f5a6fb38d13
akshay2s <akshay.aadrika@gmail.com>
2024-11-30 13:50:54 +0530

    fsdf

---
+++ tc_app_build/assets/FormProvider-SX72oztE.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-f2OjXAWC.js";import{a as s,b as a}from"./formik.esm-BnzrXcQN.js";function i({children:e,formik:r,className:o}){return t.jsx(s,{value:r,children:t.jsx(a,{onSubmit:r==null?void 0:r.handleSubmit,className:o,children:e})})}export{i as F};
