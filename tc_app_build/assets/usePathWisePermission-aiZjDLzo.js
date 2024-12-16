7d73d8d3d51c2cfceb4473d8d02e1cf279d2b664
akshay2s <akshay.aadrika@gmail.com>
2024-12-16 15:12:25 +0530

    fsdfsd

---
+++ tc_app_build/assets/usePathWisePermission-aiZjDLzo.js
@@ -0,0 +1,1 @@
+import"./router-vSHQYlWC.js";import{c as t}from"./index-tJbU7ayB.js";function f(p){var n,u;const o=t(),r=(u=(n=o==null?void 0:o.userRoutes)==null?void 0:n.map(s=>s==null?void 0:s.pages))==null?void 0:u.flat(1);return{result:r==null?void 0:r.find(s=>(s==null?void 0:s.route)==p)}}export{f as u};
