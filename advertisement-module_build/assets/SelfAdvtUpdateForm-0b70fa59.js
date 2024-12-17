91c5b29114a3c5a01cf56f4602f0cee25e934cbf
akshay2s <akshay.aadrika@gmail.com>
2024-12-17 12:12:14 +0530

    changes

---
+++ advertisement-module_build/assets/SelfAdvtUpdateForm-0b70fa59.js
@@ -0,0 +1,1 @@
+import{k as a,j as d}from"./index-4d407fc7.js";function t(){const{id:e}=a();return console.log("can edit param received id",e),d("div",{children:"SelfAdvtUpdateForm"})}export{t as default};
